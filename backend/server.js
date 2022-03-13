16763ed999b2ba0d8e8885596bbb9659a7e18fac
andre <ahochschulte.apple@gmail.com>
2022-03-13 11:05:19 +0100

    Project setup

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
